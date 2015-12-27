64ba147155b35cf72d1a4ecd9db0b7a706f86816
Jananni <jananni@gmail.com>
2015-12-26 22:20:14 -0800

    Initial Commit

---
+++ Task8/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Task8
+//
+//  Created by Jananni Rathnagiri on 12/26/15.
+//  Copyright © 2015 jananni rathnagiri. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
